02a322b9bcd9d3b745c6fb8e6cf237e935a6409e
Mehtab918 <khanmehtab4@gmail.com>
2025-03-18 02:38:56 +0530

    commit

---
+++ 07-Loops.js
@@ -0,0 +1,83 @@
+// for loop
+//print n to n-1 using for loop
+for(let i=0; i<10; i++)
+{
+    console.log(i);
+}
+
+
+// SUM OF N NUMBERS USING FOR LOOP
+
+let sum =0 ;
+
+let n= 4;
+for(let i=0; i<n; i++)
+{
+    sum = sum + (i+1);
+}
+console.log("sum of the first " + " " +n +" " + "numbers is :"+ sum );
+
+
+//for in 
+
+const restaurant = {
+    name:"Burger King",
+    location:"New York",
+    cuisine:"Burgers"
+}
+
+for (let key in restaurant)
+{
+    console.log(key + ":" + restaurant[key]);
+}
+//-----------------------------------------------------------
+const fruits = {
+jharkhand :"mango",
+bihar :" ganna",
+punjab :" lichi",
+kerela : "guava"
+}
+
+for(let a in fruits)
+{
+    console.log( " fruits in " + a + " : " + fruits[a]);
+}
+//====================================================================
+const object = { a: 1, b: 2, c: 3 };
+
+for (const property in object) {
+  console.log(`${property}: ${object[property]}`);
+}
+
+// Expected output:
+// "a: 1"
+// "b: 2"
+// "c: 3"
+
+
+
+//for of  - Receives a value from the sequence on each iteration.
+
+let arr = [1,2,3,4,5];
+
+for (let i of arr)
+{
+    console.log(i);
+}
+
+const  name = "abcdefgh";
+
+for(let char of name)
+{
+    console.log(char);
+}
+
+const array1 = ["a", "b", "c"];
+
+for (const element of array1) {
+  console.log(element);
+}
+
+// Expected output: "a"
+// Expected output: "b"
+// Expected output: "c"
